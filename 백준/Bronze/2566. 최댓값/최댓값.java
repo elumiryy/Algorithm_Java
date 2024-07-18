aa6bdb92cd13de6734d6438d7c8f74801b314b84
elumiryy <167880903+elumiryy@users.noreply.github.com>
2024-07-19 00:01:13 +0900

    [Bronze III] Title: 최댓값, Time: 84 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,6 +8,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         for (int i = 0; i < 9; i++) {
             st = new StringTokenizer(br.readLine());
@@ -23,7 +23,8 @@ public class Main {
             }
         }
 
-        System.out.println(max);
-        System.out.println((x + 1) + " " + (y + 1));
+        sb.append(max).append("\n").append(x + 1).append(" ").append(y + 1);
+        bw.write(String.valueOf(sb));
+        bw.flush();
     }
 }
